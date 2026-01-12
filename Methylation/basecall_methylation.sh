e8cae6e0a142d9eceff1ca2e255c2404368824b9
Saranya Balachandran <saranya.balachandran13@gmail.com>
2026-01-12 10:06:43 +0100

    Updated GPU usage

--- Methylation/basecall_methylation.sh
+++ Methylation/basecall_methylation.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-#SBATCH --mem=128GB
-#SBATCH --partition=shortterm
+#SBATCH --mem=490000
+#SBATCH --partition=longterm
 #SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:ampere_a40:2
 
 ## https://nanoporetech.github.io/modkit/intro_pileup.html #description-of-bedmethyl-output
 module load singularity
 module load nextflow
+module load nvidia-cuda/12.8
 export NXF_SINGULARITY_HOME_MOUNT=true
 export SINGULARITY_CACHEDIR="$WORK/singularity"
 
+
 singularity pull docker://saranyabalachandr/dorado_1.0.4_hac_v5.2:v1
-#singularity pull docker://saranyabalachandr/dorado_0.8_sup:v1
 
 singularity exec --nv docker://saranyabalachandr/dorado_1.0.4_hac_v5.2:v1 /dorado-1.0.4-linux-x64/bin/dorado basecaller "/dorado_models/dna_r10.4.1_e8.2_400bps_hac@v5.2.0" . --device auto --min-qscore 15 --recursive --reference /data/humangen_external/Nanopore/reference/hg38_ebv.fa --max-reads 100000000 --kit-name "SQK-LSK114" --trim adapters /Volumes/Humangenetik/Data/Nanopore/Daniela/BL-CL/MakuP5Monarch/20241113_1056_P2S-01141-B_PBA60521_7fe828ca/pod5/ > MakuP5Monarch.bam
+
 # for SUP models run below
+#singularity pull docker://saranyabalachandr/dorado_0.8_sup:v1
 #singularity exec --nv docker://saranyabalachandr/dorado_0.8_sup:v1 /dorado-0.8.1-linux-x64/bin/dorado basecaller "/dorado_models/dna_r10.4.1_e8.2_400bps_sup@v4.3.0" . --device auto --min-qscore 15 --recursive --reference /data/humangen_external/Nanopore/reference/hg38_ebv.fa --max-reads 100000000 --kit-name "SQK-LSK114" --trim adapters /Volumes/Humangenetik/Data/Nanopore/Daniela/BL-CL/MakuP5Monarch/20241113_1056_P2S-01141-B_PBA60521_7fe828ca/pod5/ > MakuP5Monarch.bam
