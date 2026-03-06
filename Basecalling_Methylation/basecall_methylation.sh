142292b35d8ea99105d3bcd7dc2188f83877b95e
Saranya-Balachandran <saranya.balachandran13@gmail.com>
2026-03-06 13:35:55 +0100

    Updated basecalling environment to dorado 1.4

--- Basecalling_Methylation/basecall_methylation.sh
+++ Basecalling_Methylation/basecall_methylation.sh
@@ -1,9 +1,11 @@
+#creation_date:06.03.2026
+
 #!/bin/bash
 
 #SBATCH --mem=490000
 #SBATCH --partition=longterm
 #SBATCH --cpus-per-task=32
-#SBATCH --gres=gpu:ampere_a40:2
+#SBATCH --gres=gpu:1
 
 ## https://nanoporetech.github.io/modkit/intro_pileup.html #description-of-bedmethyl-output
 module load singularity
@@ -13,10 +15,6 @@ export NXF_SINGULARITY_HOME_MOUNT=true
 export SINGULARITY_CACHEDIR="$WORK/singularity"
 
 
-singularity pull docker://saranyabalachandr/dorado_1.0.4_hac_v5.2:v1
+#replace the model based on https://software-docs.nanoporetech.com/dorado/latest/models/list/
 
-singularity exec --nv docker://saranyabalachandr/dorado_1.0.4_hac_v5.2:v1 /dorado-1.0.4-linux-x64/bin/dorado basecaller "/dorado_models/dna_r10.4.1_e8.2_400bps_hac@v5.2.0" . --device auto --min-qscore 15 --recursive --reference /data/humangen_external/Nanopore/reference/hg38_ebv.fa --max-reads 100000000 --kit-name "SQK-LSK114" --trim adapters /Volumes/Humangenetik/Data/Nanopore/Daniela/BL-CL/MakuP5Monarch/20241113_1056_P2S-01141-B_PBA60521_7fe828ca/pod5/ > MakuP5Monarch.bam
-
-# for SUP models run below
-#singularity pull docker://saranyabalachandr/dorado_0.8_sup:v1
-#singularity exec --nv docker://saranyabalachandr/dorado_0.8_sup:v1 /dorado-0.8.1-linux-x64/bin/dorado basecaller "/dorado_models/dna_r10.4.1_e8.2_400bps_sup@v4.3.0" . --device auto --min-qscore 15 --recursive --reference /data/humangen_external/Nanopore/reference/hg38_ebv.fa --max-reads 100000000 --kit-name "SQK-LSK114" --trim adapters /Volumes/Humangenetik/Data/Nanopore/Daniela/BL-CL/MakuP5Monarch/20241113_1056_P2S-01141-B_PBA60521_7fe828ca/pod5/ > MakuP5Monarch.bam
+singularity exec --nv docker://saranyabalachandr/dorado_1.4.0:v1 /dorado-1.4.0-linux-x64/bin/dorado basecaller "/dorado_models/dna_r10.4.1_e8.2_400bps_hac@v5.2.0" . --device auto --min-qscore 15 --recursive --reference /data/humangen_external/Nanopore/reference/hg38_ebv.fa --max-reads 100000000 --kit-name "SQK-LSK114" --trim adapters /Volumes/Humangenetik/Data/Nanopore/Daniela/BL-CL/MakuP5Monarch/20241113_1056_P2S-01141-B_PBA60521_7fe828ca/pod5/ > MakuP5Monarch.bam
